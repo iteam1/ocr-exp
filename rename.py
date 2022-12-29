50efb00aba449089915f7ad2add39814976c799e
locchuong <locchuong9@gmail.com>
2022-12-29 17:59:47 +0700

    update

---
+++ rename.py
@@ -0,0 +1,14 @@
+import os
+
+dir = input("Enter your directory: ")
+prefix = input("Enter your prefix: ")
+ 
+files = os.listdir(dir)
+
+for i,f in enumerate(files):
+    if ".jpg" in f:
+    	new_name = prefix+"_"+str(i)+".jpg"
+    	os.rename(os.path.join(dir,f),os.path.join(dir,new_name))
+    	print("Rename " + f + " to " + new_name)
+    else:
+    	pass
