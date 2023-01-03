312321789f4b5bd097fa9447721f68c4b808003f
locchuong <locchuong9@gmail.com>
2023-01-03 18:00:41 +0700

    update

--- rename.py
+++ rename.py
@@ -7,8 +7,8 @@ files = os.listdir(dir)
 
 for i,f in enumerate(files):
     if ".jpg" in f:
-    	new_name = prefix+"_"+str(i)+".jpg"
-    	os.rename(os.path.join(dir,f),os.path.join(dir,new_name))
-    	print("Rename " + f + " to " + new_name)
+        new_name = prefix+"_"+str(i)+".jpg"
+        os.rename(os.path.join(dir,f),os.path.join(dir,new_name))
+        print("Rename " + f + " to " + new_name)
     else:
     	pass
